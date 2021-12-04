ced5df8a265779a4634660285da1f46b87c9c755
t1ma <180107247@stu.sdu.edu.kz>
2021-12-04 23:57:09 +0600

    skin for mc
    toporist

---
+++ Assets/rum.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rum : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animation anim;
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animation>();
+        anim["spin"].layer = 123;
+    }
+ 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            anim.Play("rum");
+        }
+    }
+}
