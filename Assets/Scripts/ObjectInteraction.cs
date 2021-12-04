4674e804cd2d2dbe7d74f181b5244b0729242cb6
PolyAtomicIon <180107265@stu.sdu.edu.kz>
2021-12-04 16:21:54 +0600

    check if object is near

--- Assets/Scripts/ObjectInteraction.cs
+++ Assets/Scripts/ObjectInteraction.cs
@@ -8,8 +8,17 @@ public class ObjectInteraction : MonoBehaviour
 {
 
     Ray ray;
-    RaycastHit hit;
-    Interactable interactableItem, lastInteractableItem;
+    RaycastHit clickedObject, objectInRadius;
+    Interactable interactableObject, lastInteractableObject;
+    Vector3 interactingObjectPosition;
+    float interactingObjectHeight;
+    Transform mainCharacterTransform;
+
+    public float radius = 2.5f;
+
+    private void Start() {
+        mainCharacterTransform = GameObject.FindWithTag("Player").transform;
+    }
 
     void Update()
     {
@@ -19,26 +28,53 @@ public class ObjectInteraction : MonoBehaviour
         
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         
-        if(Physics.Raycast(ray, out hit))
+        if(Physics.Raycast(ray, out clickedObject))
         {
-            Debug.Log(hit.collider.name);
-            
-            interactableItem = hit.collider.GetComponent<Interactable>();
-            if( interactableItem != null ){
-                lastInteractableItem = interactableItem;
-                if(Input.GetMouseButton(0)){
-                    interactableItem.Interact(new Color(255, 0, 0));
-                } 
+            Debug.Log(clickedObject.collider.name);
+            interactingObjectPosition = clickedObject.collider.bounds.center;
+            interactingObjectHeight = clickedObject.collider.bounds.size.y;
+            if( isInteractable() ){
+                interactableObject = clickedObject.collider.GetComponent<Interactable>();
+                if( interactableObject != null ){
+                    lastInteractableObject = interactableObject;
+                    if(Input.GetMouseButton(0)){
+                        interactableObject.Interact(new Color(255, 0, 0));
+                    } 
+                    else {
+                        interactableObject.Interact(new Color(0, 255, 0));
+                    }
+                }
                 else {
-                    interactableItem.Interact(new Color(0, 255, 0));
+                    ResetInteractableObject();
                 }
             }
-            else {
-                Debug.Log("Disabling interaction");
-                lastInteractableItem.DisableInteraction();
-                lastInteractableItem = null;
+            else{
+                ResetInteractableObject();
             }
         } 
-            
+
+    }
+
+    void ResetInteractableObject(){
+        Debug.Log("Disabling interaction");
+        interactingObjectPosition = Vector3.zero;
+        if( lastInteractableObject != null ){
+            lastInteractableObject.DisableInteraction();
+            lastInteractableObject = null;
+        }
+    }
+
+    bool isInteractable(){
+        float distanceToMainCharacter = Vector3.Distance(interactingObjectPosition, mainCharacterTransform.position);
+        // Debug.Log(distanceToMainCharacter);
+        return ( radius >= distanceToMainCharacter );
+    }
+
+     void OnDrawGizmos()
+    {
+        if( interactingObjectPosition != Vector3.zero ){
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(interactingObjectPosition, radius);
+        }
     }
 }
