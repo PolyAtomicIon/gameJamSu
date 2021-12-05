bc40ad33832c5549253cbab89917831cec19033a
t1ma <180107247@stu.sdu.edu.kz>
2021-12-05 06:16:51 +0600

    audio
    audio

---
+++ Assets/music.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class music : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private AudioSource audio;
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            audio.Play();
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
