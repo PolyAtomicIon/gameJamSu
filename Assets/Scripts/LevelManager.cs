663228731af725f0d2ff72e7924d3e390d5b8e40
PolyAtomicIon <180107265@stu.sdu.edu.kz>
2022-01-20 01:37:55 +0600

    feat.changed 3d object to 2d marker - waypoiny

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
@@ -11,15 +12,24 @@ public class LevelManager : MonoBehaviour
     public BaseInteractable[] interactableItems;
     public int currentInteractableItem = 0;
     
-    public GameObject diamond;
-    public Material unactive, active;
-    Renderer rd;
+    public Color unactive, active;
+
+     // Indicator icon
+    public Image img;
+    public Graphic imgColor;
+    // The target (location, enemy, etc..)
+    public Transform target;
+    // UI Text to display the distance
+    public Text meter;
+    // To adjust the position of the icon
+    public Vector3 offset;
+    public Vector3 targetPosition;
 
     public void Start() {
         currentInteractableItem = 0;
         EnableInteractableObject();
-        rd = diamond.GetComponent<Renderer>();
-        rd.material = unactive;
+        // imgColor = img.GetComponent<Graphic>();
+        img.color = unactive;
     }
 
     void MoveToNextInteractableObject() {
@@ -43,9 +53,8 @@ public class LevelManager : MonoBehaviour
 
     void SetDiamondPosition(){
         Vector3 pos = interactableItems[currentInteractableItem].GetCenter();
-        Debug.Log(pos);
         pos.y += 2.25f;
-        diamond.transform.position = pos;
+        targetPosition = pos;
     }
 
     public void Update() {
@@ -55,15 +64,41 @@ public class LevelManager : MonoBehaviour
                     MoveToNextInteractableObject();
                 }
                 if( interactableItems[currentInteractableItem].isInteractable() ){
-                    if( rd != null )
-                        rd.material = active;
+                    if( img != null ){
+                        img.color = active;
+                    }
                 }else {
-                    if( rd != null )
-                        rd.material = unactive;
+                    if( img != null ){
+                        img.color = unactive;
+                    }
                 }
             }
         }
-    }
+        
+        // Giving limits to the icon so it sticks on the screen
+        // Below calculations witht the assumption that the icon anchor point is in the middle
+        // Minimum X position: half of the icon width
+        float minX = img.GetPixelAdjustedRect().width / 2;
+        // Maximum X position: screen width - half of the icon width
+        float maxX = Screen.width - minX;
 
+        // Minimum Y position: half of the height
+        float minY = img.GetPixelAdjustedRect().height / 2;
+        // Maximum Y position: screen height - half of the icon height
+        float maxY = Screen.height - minY;
+
+        // Temporary variable to store the converted position from 3D world point to 2D screen point
+        Vector2 pos = Camera.main.WorldToScreenPoint(targetPosition + offset);
+
+        // Limit the X and Y positions
+        pos.x = Mathf.Clamp(pos.x, minX, maxX);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+        // Update the marker's position
+        img.transform.position = pos;
+        // Change the meter text to the distance with the meter unit 'm'
+        // meter.text = ((int)Vector3.Distance(target.position, transform.position)).ToString() + "m";
+
+    }
 
 }
